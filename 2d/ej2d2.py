cc26a69be053183333d0b609359e900b6fd55c93
gbonillo463 <gbonillo@uoc.edu>
2026-01-10 14:42:42 +0100

    ej 2d2 COMPLETADO

--- 2d/ej2d2.py
+++ 2d/ej2d2.py
@@ -31,49 +31,33 @@ Ejemplo:
     Salida:
         ValueError
 
-Enunciat:
-Implementa la funció 'calculate_max_and_min' que rebi com a paràmetre una
-llista de números 'list_numbers', s'han de considerar els casos en què les
-dades que es troben dins de la llista siguin de tipus string o que la llista
-estigui buida.
-
-Addicionalment, la funció ha d'anar imprimint quin és el name menor i el
-name més gran segons va avançant a la llista sempre que aquest sigui diferent
-a l'anterior resultat simulant la depuració per traces.
-
-I finalment, retornar un valor del name més petit i del name més gran.
-
-Paràmetres:
-list_numbers: Llista de números.
-
-Exemple:
-     Entrada:
-         [10, 5.1, 0, -2, 31, 55, 70, -10, 200, -55.55]
-     Sortida:
-         'Greater:' 200
-         'Lesser: ' -55.55
-
-     Entrada:
-         ['Hello', 1, 5, -20, 55.5]
-     Sortida:
-         TypeError
-
-     Entrada:
-         []
-     Sortida:
-         ValueError
-
 """
 
 
 def calculate_max_and_min(list_numbers):
-    # Write here your code
-    pass
+    try:
+        max_value = list_numbers[0]
+        min_value = list_numbers[0]
+        for number in list_numbers:
+            if number > max_value:
+                max_value = number
+            print(f'Greater: {max_value}')
+    
+            if number < min_value:
+                min_value = number
+            print(f'Lesser: {min_value}')
+
+    except TypeError:
+        return 'Only a list of numbers is allowed'
+    except ValueError:
+        return 'List format error'
+
+    return min_value, max_value 
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 
-# print(
-#     "\nResult: ", calculate_max_and_min([10, 5.1, 0, -2, 31, 55, 70, -10, 200, -55.55])
-# )
+print(
+    "\nResult: ", calculate_max_and_min([10, 5.1, 0, -2, 31, 55, 70, -10, 200, -55.55])
+)
